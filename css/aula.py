a04bca587da59f5a0bded0b41165cf553d049c90
Eric Martins <ericgaidargimartins@gmail.com>
2026-02-11 15:31:39 -0300

    Projeto Finalizado!

--- css/aula.py
+++ css/aula.py
@@ -1,5 +1,5 @@
-import math
-co = float(input('Digite o valor do cateto oposto: '))
-ca = float(input('Digite o valor do cateto adjacente: '))
-h = math.hypot(co, ca)
-print('A hipotenusa é {}' .format(h))
+python -m pip install pygame
+import pygame
+print(pygame.ver)
+
+
--- script.js
+++ script.js
@@ -5,11 +5,11 @@ function toggleMode() {
     const img = document.querySelector("#profile img")
 
     if(html.classList.contains('light')){
-        img.setAttribute('src', './assets/avatar-light.png')
-        img.setAttribute('alt', 'Homem branco com óculos escuro, jaqueta cinza, camiseta preta por baixo, e sem barba!')
+        img.setAttribute('src', './assets/avatar2-light.png')
+        img.setAttribute('alt', 'Jovem com camiseta preta, fundo escuro e leve iluminação verde!')
     } else {
-       img.setAttribute('src', './assets/avatar.png')
-       img.setAttribute('alt', 'Foto de Mayk Brito sorrindo, usando óculos e camisa preta, barba e fundo amarelo.')
+       img.setAttribute('src', './assets/avatar1.png')
+       img.setAttribute('alt', 'Jovem de camisa preta, com um fundo cinza em tons de amarelo.')
     }
    
 }
